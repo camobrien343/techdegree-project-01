272e51de4fee394e53050853cbcac6b2193aee61
Cam O'Brien <camobrien343@gmail.com>
2018-01-07 17:09:42 -0600

    Worked on constructing Random Quote Generator - needs more work

--- js/script.js
+++ js/script.js
@@ -1,4 +1,61 @@
+/* Treehouse Techdegree
+Project 01
+Random Quote Generator
+*/
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// Global variables
+var quotes = [ {
+  quote : "The most beautiful experience we can have is the mysterious. It is the fundamental emotion that stands at the cradle of true art and true science.",
+  source : "Albert Einstein",
+  citation : "The World As I See It",
+  date : "1931"
+}, {
+  quote : "Everything you can imagine is real.",
+  source : "Pablo Picasso",
+  citation : "",
+  date : ""
+}, {
+  quote : "Art is not what you see, but what you make others see.",
+  source : "Edgar Degas",
+  citation : "",
+  date : ""
+}, {
+  quote : "Poo-tee-weet?",
+  source : "Kurt Vonnegut",
+  citation : "Slaughterhouse-Five",
+  date : ""
+}, {
+  quote : "The bulk of the world’s knowledge is an imaginary construction.",
+  source : "Hellen Keller",
+  citation : "The Five-sensed World",
+  date : "1910"
+} ];
+
+
+// Random Quote function created using random object in quotes array
+function getRandomQuote() {
+  var randomNumber = quotes[Math.floor(Math.random()*quotes.length)];
+  return randomNumber;
+}
+
+// Function to print random quote 
+function printQuote() {
+  var printQuote = getRandomQuote();
+  if ( printQuote.date !== "" ) { // Checks to see if there's value in citation property
+    document.getElementById('quote-box').innerHTML =
+    '<p class="quote">' + printQuote.quote + '</p>' + '<p class="source">' + printQuote.source + '<span class="citation">' + printQuote.citation + '</span>' + '<span class="year">' + printQuote.date + '</span></p>';
+  } else if ( printQuote.citation !== "" ) {
+    document.getElementById('quote-box').innerHTML =
+    '<p class="quote">' + printQuote.quote + '</p>' + '<p class="source">' + printQuote.source + '<span class="citation">' + printQuote.citation + '</span></p>';
+  } else {
+    document.getElementById('quote-box').innerHTML =
+    '<p class="quote">' + printQuote.quote + '</p>' + '<p class="source">' + printQuote.source + '</p>';
+  }
+}
+
+// Calls the printQuote function 
+printQuote();
