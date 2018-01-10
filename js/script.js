036d4e8ab99b6d19d9741f805f9047f5378a8e45
Cam O'Brien <camobrien343@gmail.com>
2018-01-10 11:56:42 -0600

    Changed my printQuote function and added tags and timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /* Treehouse Techdegree
+Cameron O'Brien
 Project 01
 Random Quote Generator
 */
@@ -8,33 +9,36 @@ Random Quote Generator
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 // Global variables
-let quotes = [ {
+const quotes = [ {
   quote : "The most beautiful experience we can have is the mysterious. It is the fundamental emotion that stands at the cradle of true art and true science.",
   source : "Albert Einstein",
   citation : "The World As I See It",
-  date : "1931"
+  date : "1931",
+  tags: ["Smart"]
 }, {
   quote : "Everything you can imagine is real.",
   source : "Pablo Picasso",
-  citation : "",
-  date : ""
+  tags: ["Art"]
 }, {
   quote : "Art is not what you see, but what you make others see.",
   source : "Edgar Degas",
-  citation : "",
-  date : ""
+  tags: ["Art", "Good"],
 }, {
   quote : "Poo-tee-weet?",
   source : "Kurt Vonnegut",
   citation : "Slaughterhouse-Five",
-  date : ""
+  tags: ["Author", "Kilgore Trout"]
+}, {
+  quote : "It is easier to stay out than get out.",
+  source : "Mark Twain",
+  tags: ["Author", "Knowledge"]
 }, {
   quote : "The bulk of the world’s knowledge is an imaginary construction.",
   source : "Hellen Keller",
   citation : "The Five-sensed World",
-  date : "1910"
+  date : "1910",
+  tags: ["Author"]
 } ];
-
 
 // Random Quote function created using random object in quotes array
 function getRandomQuote() {
@@ -42,41 +46,33 @@ function getRandomQuote() {
   return randomNumber;
 }
 
-// Function to print random quote 
-function printQuote() {
-  let printQuote = getRandomQuote();
-  if ( printQuote.date !== "" ) { // Checks to see if there's value in citation property
-    document.getElementById('quote-box').innerHTML = `
-    <p class="quote">${printQuote.quote}</p>
-    <p class="source">${printQuote.source}
-    <span class="citation">${printQuote.citation}</span>
-    <span class="year">${printQuote.date}</span></p>
-    `;
-  } else if ( printQuote.citation !== "" ) {
-    document.getElementById('quote-box').innerHTML =`
-    <p class="quote">${printQuote.quote}</p>
-    <p class="source">${printQuote.source}
-    <span class="citation">${printQuote.citation}</span></p>
-    `;
-  } else {
-    document.getElementById('quote-box').innerHTML =`
-    <p class="quote">${printQuote.quote}</p>
-    <p class="source">${printQuote.source}</p>
-    `;
-  }
-  document.getElementById('randomColor').style.backgroundColor = random_bg_color();
+// This setInterval will change the displayed quote every 30 seconds
+setInterval(printQuote, 30000);
+
+// Function to create random RGB Values to change background color and store in variable randomColor
+function random_bg_color() {
+    let color1 = Math.floor(Math.random() * 256);
+    let color2 = Math.floor(Math.random() * 256);
+    let color3 = Math.floor(Math.random() * 256);
+    let randomColor = "rgb(" + color1 + "," + color2 + "," + color3 + ")";
+    return randomColor;
 }
 
-function random_bg_color() {
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    let randomColor = "rgb(" + x + "," + y + "," + z + ")";
-    console.log(randomColor);
-  
-    document.body.style.background = randomColor;
-    }
-
-
-// Calls the printQuote function 
-printQuote();
+// Function to construct random quote and insert into page
+function printQuote() {
+  let printRandomQuote = getRandomQuote(); //Calls getRandomQuote function and stores in variable printRandomQuote
+  let message = '<p class="quote">' + printRandomQuote.quote + '</p>';
+  message += '<p class="source">' + printRandomQuote.source;
+  if ( printRandomQuote.citation !== undefined && printRandomQuote.date !== undefined ) {
+    message += '<span class="citation">' + printRandomQuote.citation + '</span>'; 
+    message += '<span class="year">' + printRandomQuote.date + '</span>';
+  } else if (printRandomQuote.citation !== undefined){
+    message += '<span class="citation">' + printRandomQuote.citation + '</span>'; 
+  } else if (printRandomQuote.date !== undefined){
+    message += '<span class="year">' + printRandomQuote.date + '</span>'; 
+  }
+  message += '</p>';
+  message += '<p class="tags">tags: ' + printRandomQuote.tags.join(", ") + '</p>';
+  document.body.style.backgroundColor = random_bg_color(); // Changes the background color to random color when printQuote is called
+  document.getElementById('quote-box').innerHTML = message;
+}
